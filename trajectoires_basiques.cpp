a88f66a1032db4cd2fda8172831258a1f275d116
CrazyR99 <63822097+CrazyR99@users.noreply.github.com>
2020-04-17 16:09:43 +0200

    Update trajectoires_basiques.cpp
    Ajout de l'écriture à la fin, et début de la lecture au début

--- trajectoires_basiques.cpp
+++ trajectoires_basiques.cpp
@@ -1,10 +1,12 @@
-const byte moteur_d = A1;
-const byte moteur_g = A2;
+const byte moteur_d = A1; //envoie du PWM au moteur droit
+const byte moteur_g = A2; //envoie du PWM au moteur gauche
+const byte mesure_d = A3; //reception en PWM de la vitesse mesurée à droite
+const byte mesure_g = A4; //reception en PWM de la vitesse mesurée à gauche
 
 const float e = 0; //Valeur à trouver
 const float a_max = 0.5;//   en m/s^2
 const float v_max = 1.0; //  en m/s
-const float pi = 3.1416
+const float pi = 3.1416;
 
 //Besoin de ces 3 infos pour se repérer dans l'espace de façon orientée
 float x_veh;
@@ -19,6 +21,8 @@ float delta_y;
 //Vitesses doivent toujours etre dispo pour qu'on puisse y acceder quand on veut
 float vg;
 float vd;
+float vd_mes;
+float vg_mes;
 
 //Variables utiles pour plusieurs Headers
 int etat; //sert à effectuer chaque fonction sans inhiber le reste du programme tout en connaissant l'état actuel des actions menées
@@ -27,7 +31,6 @@ int sens; //sert pour les différentiels entre les roues. 0=droite et 1=gauche
 
 #include "demi_cercle.h"
 #include "pivot.h"
-#include "ligne_droite.h"
 
 
 void setup() {
@@ -45,18 +48,29 @@ void setup() {
 }
 
 void loop() {
-    //PARTIE LECTURE où Aziz et Guillaume envoient x_cons, y-cons et theta_cons
+    
+    //Si etat = 0 on lit, sinon on ignore la commande
+    //PARTIE LECTURE (où Aziz et Guillaume envoient x_cons, y-cons et theta_cons et Alexis et Charlie envoient les mesures)
     //Si theta = 0, on en conclut que c'est un ligne droite ou un demi cercle, sinon c'est un pivot d'angle theta
-
+    
+    vd_mes = analogRead(A3);
+    vg_mes = analogRead(A4);
+    
+    //PARTIE CALCUL
+    //Penser à rajouter un PID dans chaque prgm
     if (theta != 0) {
         etat = pivot(&theta, &vg, &vd);
     }
 
-    else if ((cos(angle_veh)*sqrt((x_cons-x_veh)*(xcons-x_veh)+(y_cons-y-veh)*(y_cons-y_veh)) == (x_cons-x_veh)) && (sin(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (y_cons - y_veh))){
-        etat = ligne_droite(x_cons, y_cons, x_veh, y_veh);
+    else if ((cos(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (x_cons - x_veh)) && (sin(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (y_cons - y_veh))) {
+        etat = ligne_droite(x_cons, y_cons, x_veh, y_veh, &vg, &vd);
+    }
+
+    else {
+        etat = demi_cercle(x_eh, y_veh, x_consigne, y_consigne, &vg, &vd, &sens);
     }
     
-    else{
-        etat = demi_cercle(x_veh, y_veh, angle_veh, x_consigne, y_consigne, &vg, &vd, &sens);
-    }
+    //PARTIE ECRITURE
+    analogWrite(A1, vd);
+    analogWrite(A2, vg);
 }
