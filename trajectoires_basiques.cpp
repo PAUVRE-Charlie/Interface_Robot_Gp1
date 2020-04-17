6fa93314fb3213003115b957aa9796d1543a40b9
CrazyR99 <63822097+CrazyR99@users.noreply.github.com>
2020-04-17 14:59:54 +0200

    Update trajectoires_basiques.cpp
    Ajout de ligne_droite.h et de demi_cercle.h

--- trajectoires_basiques.cpp
+++ trajectoires_basiques.cpp
@@ -1,18 +1,18 @@
 const byte moteur_d = A1;
 const byte moteur_g = A2;
 
-const float e = 0; //Valeur à trouver
+const float e = 0; //Valeur Ã  trouver
 const float a_max = 0.5;//   en m/s^2
 const float v_max = 1.0; //  en m/s
 const float pi = 3.1416
 
-//Besoin de ces 3 infos pour se repérer dans l'espace de façon orientée
+//Besoin de ces 3 infos pour se repÃ©rer dans l'espace de faÃ§on orientÃ©e
 float x_veh;
 float y_veh;
 float angle_veh;
 
-//Coordonées du prochain point à atteindre
-//On pourrait aussi rajouter une orientation si on veut, à voir
+//CoordonÃ©es du prochain point Ã  atteindre
+//On pourrait aussi rajouter une orientation si on veut, Ã  voir
 float delta_x;
 float delta_y;
 
@@ -21,12 +21,13 @@ float vg;
 float vd;
 
 //Variables utiles pour plusieurs Headers
-int etat; //sert à effectuer chaque fonction sans inhiber le reste du programme tout en connaissant l'état actuel des actions menées
-unsigned long time; //sert à effectuer des chronos à chaque fois, à rafraichir dès qu'il ne sert plus
-int sens; //sert pour les différentiels entre les roues. 0=droite et 1=gauche
+int etat; //sert Ã  effectuer chaque fonction sans inhiber le reste du programme tout en connaissant l'Ã©tat actuel des actions menÃ©es
+unsigned long time; //sert Ã  effectuer des chronos Ã  chaque fois, Ã  rafraichir dÃ¨s qu'il ne sert plus
+int sens; //sert pour les diffÃ©rentiels entre les roues. 0=droite et 1=gauche
 
 #include "demi_cercle.h"
 #include "pivot.h"
+#include "ligne_droite.h"
 
 
 void setup() {
@@ -44,12 +45,18 @@ void setup() {
 }
 
 void loop() {
-    //PARTIE LECTURE où Aziz et Guillaume envoient x_cons, y-cons et theta_cons
+    //PARTIE LECTURE oÃ¹ Aziz et Guillaume envoient x_cons, y-cons et theta_cons
     //Si theta = 0, on en conclut que c'est un ligne droite ou un demi cercle, sinon c'est un pivot d'angle theta
 
     if (theta != 0) {
         etat = pivot(&theta, &vg, &vd);
     }
 
-    else if ((cos(angle_veh)*sqrt((x_cons-x_veh)*(xcons-x_veh)+(y_cons-y-veh)*(y_cons-y_veh)) == (x_cons-x_veh)) && (sin(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (y_cons - y_veh)))
+    else if ((cos(angle_veh)*sqrt((x_cons-x_veh)*(xcons-x_veh)+(y_cons-y-veh)*(y_cons-y_veh)) == (x_cons-x_veh)) && (sin(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (y_cons - y_veh))){
+        etat = ligne_droite(x_cons, y_cons, x_veh, y_veh);
+    }
+    
+    else{
+        etat = demi_cercle(x_veh, y_veh, angle_veh, x_consigne, y_consigne, &vg, &vd, &sens);
+    }
 }
