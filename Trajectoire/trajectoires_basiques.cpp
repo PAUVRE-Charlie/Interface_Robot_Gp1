59c7e4166e967c831f1e4dbdbf43aba693bd9746
CrazyR99 <63822097+CrazyR99@users.noreply.github.com>
2020-04-22 13:18:33 +0200

    Update trajectoires_basiques.cpp

--- Trajectoire/trajectoires_basiques.cpp
+++ Trajectoire/trajectoires_basiques.cpp
@@ -1,55 +1,91 @@
-const byte moteur_d = A1;
-const byte moteur_g = A2;
-
-const float e = 0; //Valeur � trouver
+const float e = 0; //Valeur à trouver
 const float a_max = 0.5;//   en m/s^2
 const float v_max = 1.0; //  en m/s
-const float pi = 3.1416
+const float pi = 3.1416;
 
-//Besoin de ces 3 infos pour se rep�rer dans l'espace de fa�on orient�e
+//Besoin de ces 3 infos pour se repérer dans l'espace de façon orientée
 float x_veh;
 float y_veh;
 float angle_veh;
 
-//Coordon�es du prochain point � atteindre
-//On pourrait aussi rajouter une orientation si on veut, � voir
-float delta_x;
-float delta_y;
 
 //Vitesses doivent toujours etre dispo pour qu'on puisse y acceder quand on veut
 float vg;
 float vd;
+float vd_mes;
+float vg_mes;
+
+//Compteur pour lire les instructions de l'odri une par une
+int compteur;
+
 
 //Variables utiles pour plusieurs Headers
-int etat; //sert � effectuer chaque fonction sans inhiber le reste du programme tout en connaissant l'�tat actuel des actions men�es
-unsigned long time; //sert � effectuer des chronos � chaque fois, � rafraichir d�s qu'il ne sert plus
-int sens; //sert pour les diff�rentiels entre les roues. 0=droite et 1=gauche
+int etat;           //sert à effectuer chaque fonction sans inhiber le reste du programme tout en connaissant l'état actuel des actions menées
+unsigned long time; //sert à effectuer des chronos à chaque fois, à rafraichir dès qu'il ne sert plus
+int sens;           //sert pour les différentiels entre les roues. 0=droite et 1=gauche
+unsigned long delta_acc;
+unsigned long delta_plat;
+
+//Pour les PID
+unsigned long currentTime, previousTime;
+double elapsedTime;
+float output_d, output_g;
 
 #include "demi_cercle.h"
 #include "pivot.h"
+#include "ligne_droite.h"
+#include "pid.h"
 
 
 void setup() {
-
-    pinMode(A0, INPUT);
-    pinMode(A1, OUTPUT);
-    pinMode(A2, OUTPUT);
-
-    analogWrite(A2, 0);
-    analogWrite(A1, 0);
-
     etat = 0;
+    compteur = 0;
+
+    cumError_d = 0;
+    cumError_g = 0;
+    lastError_d = 0;
+    lastError_g = 0;
+    previousTime = millis();
 
     Serial.begin(9600);
 }
 
 void loop() {
-    //PARTIE LECTURE o� Aziz et Guillaume envoient x_cons, y-cons et theta_cons
-    //Si theta = 0, on en conclut que c'est un ligne droite ou un demi cercle, sinon c'est un pivot d'angle theta
 
-    if (theta != 0) {
-        etat = pivot(&theta, &vg, &vd);
+    //PARTIE LECTURE
+      //Kamil et Seb lisent le tableau de Aziz et Guillaume (l'identifiant id et l'ordre o) quand il faut
+      //Alexis et Charlie update les mesures ici, et les stockent dans vd_mes et vg_mes
+
+
+
+  
+    
+      
+    //PARTIE CALCUL
+    
+    if (id == 0) {
+        etat = pivot(o, &vg, &vd);
     }
 
-    else if ((cos(angle_veh)*sqrt((x_cons-x_veh)*(xcons-x_veh)+(y_cons-y-veh)*(y_cons-y_veh)) == (x_cons-x_veh)) && (sin(angle_veh) * sqrt((x_cons - x_veh) * (xcons - x_veh) + (y_cons - y - veh) * (y_cons - y_veh)) == (y_cons - y_veh)))
+    else if (id == 1) {
+        etat = ligne_droite(o, &vg, &vd);
+    }
+
+    else {
+        etat = demi_cercle(o, &vg, &vd);
+    }
+    
+   
+    
+    //RECTIFICATION PID
+    currentTime = millis();                                        
+    elapsedTime = (float)((currentTime - previousTime) / 1000);    
+    pid(vg_mes, vd_mes, &vg, &vd, elapsedTime);
+    previousTime = currentTime;                        
+    
+    
+                                                       
+    
+    //PARTIE ECRITURE
+    //A compléter par Charlie et Alexis
 }
