221f08c5364691f9520b8ea183f762a56ca0ef21
sella <sellaziz@hotmail.fr>
2020-04-23 20:43:16 +0200

    ajout de commentaires

--- Interface Graphique/Interface_v2.py
+++ Interface Graphique/Interface_v2.py
@@ -18,19 +18,18 @@ class Interface():
     DRAW_METHOD = 1
 
     def __init__(self):
-
+        ## Initialisation de Tkinter
         self.root = tkinter.Tk()
         self.root.wm_title("Interface Commande Robot")
 
         # Variables
-        self.plt_draw = np.zeros((1,2))
+        self.plt_draw = np.zeros((1,2)) # Coordonnées interprété par matplotlib pour le tracé
         self.plt_draw[0] = np.array([0,1])
         self.command = np.zeros((1,2))
         self.command[0] = np.array([1,1])
         self.directions = np.array([[0,1]])
 
         self.setup()
-        # print(self.plt_draw,"\n",self.plt_draw[:,0], self.plt_draw[:,1])
         self.drawing, = self.ax.plot(self.plt_draw[:, 0], self.plt_draw[:, 1])
 
         self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
@@ -39,6 +38,7 @@ class Interface():
         self.set_buttons()
 
     def setup(self):
+        """Mise en place de la Figure Matplotlib"""
         self.fig = Figure(figsize=self.FIGSIZE, dpi=self.DPI)
         self.ax = self.fig.add_subplot(111)
 
@@ -50,6 +50,7 @@ class Interface():
         self.toolbar.update()
 
     def set_buttons(self):
+        """Mise en place des boutons sur Tkinter"""
         self.method_entry = tkinter.Entry(self.root)
         self.method_entry.pack(side=tkinter.TOP)
 
@@ -63,22 +64,26 @@ class Interface():
         self.quit_button.pack(side=tkinter.BOTTOM)
 
     def update_method(self):
+        """Tempoiraire, c'est pour mettre à jour la méthode de tracé, ici 1 est la ligne droite"""
         self.DRAW_METHOD = self.method_entry.get()
         print(self.DRAW_METHOD)
 
     def on_key_press(self, event):
+        """Gestion de l'évènement clic gauche et gestion du tracé"""
         print("you pressed ({0},{1})".format(event.xdata, event.ydata))
         key_press_handler(event, self.canvas, self.toolbar)
 
-        if self.DRAW_METHOD==1:
+        if self.DRAW_METHOD==1: # Ligne droite
             x, y = event.xdata, event.ydata
             dv = x-self.plt_draw[-1][0] , y-self.plt_draw[-1][1]
-            angle = self.get_ang(self.plt_draw[-1], np.array([x,y]), dir=True, direction=self.directions[-1])
-            self.directions = np.vstack((self.directions, np.array(dv / np.linalg.norm(dv))))
+            angle = self.get_ang(self.plt_draw[-1], np.array([x,y]), dir=True, direction=self.directions[-1]) # Détermination de l'angle de rotation (valeur absolue)
+            self.directions = np.vstack((self.directions, np.array(dv / np.linalg.norm(dv)))) # Mise à jour des directions deu robot
+            # Ajout des coordonnées du click
             self.plt_draw = np.vstack((self.plt_draw, np.array([x,y])))
-            if np.dot((self.directions[-1][0],0), (1,0))>0:
+            if np.dot((self.directions[-1][0],0), (1,0))>0: # Essai pour déterminer le signe de l'angle mais infructueux
                 angle = -angle
-            if angle!=0:
+            # Ajout des commandes
+            if angle!=0: # Ajoute une commande de rotation s'il est non nul
                 self.command = np.vstack((self.command, np.array([0,angle]))) # Rotation
             self.command = np.vstack((self.command, np.array([1,np.linalg.norm(dv)]))) # Ligne Droite
 
@@ -88,11 +93,8 @@ class Interface():
         self.ax.figure.canvas.draw()
 
     def get_ang(self, v1, v2, dir=False, direction=(0,1)):
+        """Détermine l'angle de rotation avec considération possible de la direction précédante"""
         vect1, vect2 = v1, v2
-        if v1[0]==0 and v1[1]==0:
-            vect1=np.array([0, 1])
-            vect2 = v2
-            dir=False
         if dir :
             vect2 = vect2-vect1
             vect1=direction
@@ -102,6 +104,7 @@ class Interface():
         return np.arccos(dot_product)
 
     def print_command(self):
+        """Imprime les commandes sur la console"""
         print(self.command)
 
     def _quit(self):
