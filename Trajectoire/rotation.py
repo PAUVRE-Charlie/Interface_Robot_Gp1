74136c6dfd2644099d7c51579429279ee44d1f6a
PAUVRE-Charlie <63854843+PAUVRE-Charlie@users.noreply.github.com>
2020-05-14 18:07:39 +0200

    Update rotation.py

--- Trajectoire/rotation.py
+++ Trajectoire/rotation.py
@@ -2,10 +2,14 @@ from time import *
 from math import *
 
 ### Contraintes techniques
+vmax=1
+amax=0.5
 wmax = 10.5   #en rad s-1
 epsmax = 12.5 #en rad.s-2
 r = 0.04      #4cm
 L = 0.3       #30cm
+#wmax=vmax/r
+#epsmax=amax/r
 
 start = 0
 
@@ -15,8 +19,7 @@ start = 0
 def rotation(angle):
     global tau_plat
     global tau
-    vdroit = open("vdroit", "w")
-    vgauche = open("vgauche", "w")
+    vrobot = open("vrobot_rot", "w")
 
     tau_plat = wmax/epsmax
     tau = sqrt(L*abs(angle)/(2*r*epsmax))
@@ -31,41 +34,36 @@ def rotation(angle):
         if angle >= 0:
 
             while time() - start < tau:
-                vdroit.write(str(127 + 127/wmax * (time()-start)*epsmax) + " ; ")
-                vgauche.write(str(127 - 127 / wmax * (time() - start) * epsmax) + " ; ")
+                vrobot.write(str((time()-start)*epsmax) + ",")
+                vrobot.write(str(-(time() - start) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
 
             start = time()
 
             while time()- start < tau:
-                vdroit.write(str(127 + 127/wmax * (tau - (time()-start))*epsmax) + " ; ")
-                vgauche.write(str(127 - 127/wmax * (tau - (time()-start))*epsmax) + " ; ")
+                vrobot.write(str((tau - (time()-start))*epsmax) + ",")
+                vrobot.write(str(-(tau - (time()-start))*epsmax) + ";")
                 sleep(0.01)
-            vdroit.write(str(127))
-            vgauche.write(str(127))
+            vrobot.write(str(0) + ",")
+            vrobot.write(str(0))
 
             return 0
 
         ###SI ON PIVOTE VERS LA DROITE
         else:
-
             while time() - start < tau:
-                vgauche.write(str(127 + 127 / wmax * (time() - start) * epsmax) + " ; ")
-                vdroit.write(str(127 - 127 / wmax * (time() - start) * epsmax) + " ; ")
+                vrobot.write(str(-(time() - start) * epsmax) + ",")
+                vrobot.write(str((time() - start) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
-
+           
             start = time()
 
             while time() - start < tau:
-                vgauche.write(str(127 + 127 / wmax * (tau - (time() - start)) * epsmax) + " ; ")
-                vdroit.write(str(127 - 127 / wmax * (tau - (time() - start)) * epsmax) + " ; ")
+                vrobot.write(str(-(tau - (time() - start)) * epsmax) + ",")
+                vrobot.write(str((tau - (time() - start)) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write(str(127))
-            vgauche.write(str(127))
+            vrobot.write(str(0) + ",")
+            vrobot.write(str(0))
 
             return 0
 
@@ -78,61 +76,54 @@ def rotation(angle):
 
         ### SI ON PIVOTE VERS LA GAUCHE ###
         if angle >= 0:
-
+            
             while time() - start < tau_plat:
-                vdroit.write(str(127 + 127 / wmax * (time() - start) * epsmax) + " ; ")
-                vgauche.write(str(127 - 127 / wmax * (time() - start) * epsmax) + " ; ")
+                vrobot.write(str((time() - start) * epsmax) + ",")
+                vrobot.write(str(-(time() - start) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
-
+           
             start = time()
 
-            while time() - start < (abs(angle) - 2*r*epsmax*tau_plat*tau_plat/L)/wmax:
-                vdroit.write(str(254) + " ; ")
-                vgauche.write(str(0) + " ; ")
+            while time() - start < (abs(angle) - 2*r*epsmax*tau_plat*tau_plat/L)/wmax*L/2/r:
+                vrobot.write(str(wmax) + ",")
+                vrobot.write(str(-wmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
-
+           
             start = time()
 
             while time() - start < tau_plat:
-                vdroit.write(str(254- 127 *(time()-start)/tau_plat) + " ; ")
-                vgauche.write(str(127 *(time()-start)/tau_plat) + " ; ")
+                vrobot.write(str(wmax-(time()-start)*epsmax) + ",")
+                vrobot.write(str(-wmax+(time()-start)*epsmax) + ";")
                 sleep(0.01)
-            vdroit.write(str(127))
-            vgauche.write(str(127))
+            vrobot.write(str(0) + ",")
+            vrobot.write(str(0))
 
             return 0
 
         ### SI ON PIVOTE VERS LA DROITE ###
         else:
-
+            #print("d")
             while time() - start < tau_plat:
-                vgauche.write(str(127 + 127 / wmax * (time() - start) * epsmax) + " ; ")
-                vdroit.write(str(127 - 127 / wmax * (time() - start) * epsmax) + " ; ")
+                vrobot.write(str(-(time() - start) * epsmax) + ",")
+                vrobot.write(str((time() - start) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
-
+           
             start = time()
 
-            while time() - start < (abs(angle) - 2 * r * epsmax * tau_plat * tau_plat / L) / wmax:
-                vgauche.write(str(254) + " ; ")
-                vdroit.write(str(0) + " ; ")
+            while time() - start < (abs(angle) - 2 * r * epsmax * tau_plat * tau_plat / L)/wmax*L/2/r:
+                #print("f")
+                vrobot.write(str(-wmax) + ",")
+                vrobot.write(str(wmax) + ";")
                 sleep(0.01)
-            vdroit.write("\n")
-            vgauche.write("\n")
-
+            
             start = time()
 
             while time() - start < tau_plat:
-                vgauche.write(str(254 - 127 * (time() - start) / tau_plat) + " ; ")
-                vdroit.write(str(127 * (time() - start) / tau_plat) + " ; ")
+                vrobot.write(str(-wmax+(time() - start) * epsmax) + ",")
+                vrobot.write(str(wmax - (time() - start) * epsmax) + ";")
                 sleep(0.01)
-            vdroit.write(str(127))
-            vgauche.write(str(127))
+            vrobot.write(str(0) + ",")
+            vrobot.write(str(0))
 
             return 0
 
