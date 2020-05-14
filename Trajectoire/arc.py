0beb75631bddbd42ac3db9ccf995177f73fdf77e
PAUVRE-Charlie <63854843+PAUVRE-Charlie@users.noreply.github.com>
2020-05-14 18:09:01 +0200

    Update arc.py

--- Trajectoire/arc.py
+++ Trajectoire/arc.py
@@ -2,8 +2,8 @@ from time import *
 from math import *
 
 ### Code de la route ###
-vmax = 1.0 #en m/s (en vrai je crois que c'est 0.42, a verifier)
-amax = 0.5 #en m/s^2 (la ca correspond a la valeur de r et epsmax
+vmax = 1 #en m/s (en vrai je crois que c'est 0.42, a verifier)
+amax = 0.5#en m/s^2 (la ca correspond a la valeur de r et epsmax
 
 ### Contraintes techniques ###
 wmax = 10.5   #en rad s-1
@@ -18,66 +18,97 @@ tau_plat = vmax / amax
 start = 0.
 
 def arc(x,y,theta,xc,yc):
-
+    print(atan(-1))
+    print(atan(1))
+    print(atan(0))
     global dx, dy
     global dx_veh, dy_veh
     global rayon, rapport, tau
 
-    vdroit = open("vdroit", "w")
-    vgauche = open("vgauche", "w")
-    essai = open("essai", "w")
+    vrobot=open("vrobot_arc","w")
 
     dx = xc-x
     dy = yc-y
 
-    dx_veh = sin(theta)*dy + cos(theta)*dx
-    dy_veh = cos(theta)*dy - sin(theta)*dx
+    dx_veh = -cos(theta)*dy + sin(theta)*dx
+    dy_veh = sin(theta)*dy + cos(theta)*dx
+
+    print(dx_veh)
+    print(dy_veh)
 
     rayon = abs((dx_veh*dx_veh+dy_veh*dy_veh)/(2*dx_veh))
     rapport = (2*rayon - L)/(2*rayon +L)
-    d_ext = abs((rayon+L/2)*atan2(dy_veh,rayon-dx_veh))
-    tau = sqrt(d_ext/amax)
+    print(rapport)
 
-    essai.write(str(rayon) + " ; " + str(rapport) + " ; " + str(d_ext))
+    if (dy_veh > 0):
+        if abs(dx_veh) == rayon:
+            d_ext = (rayon + L / 2) * pi / 2
+        elif 0 < abs(dx_veh) < rayon:
+            d_ext = (rayon + L / 2) * atan(dy_veh / (rayon - abs(dx_veh)))
+        elif abs(dx_veh) > rayon:
+            d_ext = (rayon + L / 2) * (pi + atan(dy_veh / (rayon - abs(dx_veh))))
+        else:
+            d_ext=0
+    elif dy_veh<0:
+        if abs(dx_veh) == rayon:
+            d_ext = (rayon + L / 2) * 3 * pi / 2
+        elif 0 < abs(dx_veh) < rayon:
+            d_ext = (rayon + L / 2) * (2*pi+atan(dy_veh / (rayon - abs(dx_veh))))
+        elif abs(dx_veh) > rayon:
+            d_ext = (rayon + L / 2) * (pi + atan(dy_veh / (rayon - abs(dx_veh))))
+        else:
+            d_ext=0
+    else:
+        d_ext = (rayon + L / 2) * pi
+
+    tau = sqrt(d_ext/amax)
+    print("########################")
+    print(d_ext)
+    print(tau)
+    print(vmax*vmax/amax)
+    print(tau_plat)
+
+    #essai.write(str(rayon) + " ; " + str(rapport) + " ; " + str(d_ext))
 
     ### IL Y A UN PLATEAU ###
-    if dy_veh/(rayon - dx_veh) >= tan(2*vmax*vmax/amax/(2*rayon+L)):
-
+    if d_ext >= vmax*vmax/amax:
         start = time()
 
         while time() - start < tau_plat:
             if dx_veh < 0: #On tourne a gauche
-                vdroit.write(str(127 + 127*(time()-start)*amax/vmax) + " ; ")
-                vgauche.write(str(127 + abs(rapport)*127*(time()-start)*amax/vmax) + " ; ")
+                vrobot.write(str((time()-start)*amax/r) + ",")
+                vrobot.write(str(abs(rapport)*(time()-start)*amax/r) + ";")
+                sleep(0.01)
             else:
-                vgauche.write(str(127 + 127 * (time() - start) * amax/vmax) + " ; ")
-                vdroit.write(str(127 + abs(rapport) * 127 * (time() - start) * amax/vmax) + " ; ")
-        vgauche.write("\n")
-        vdroit.write("\n")
+                vrobot.write(str(abs(rapport)*(time() - start) * amax/r) + ",")
+                vrobot.write(str((time() - start) * amax/r) + ";")
+                sleep(0.01)
 
         start = time()
 
         while time()-start < d_ext/vmax - tau_plat:
             if dx_veh < 0:  # On tourne a gauche
-                vdroit.write(str(255) + " ; ")
-                vgauche.write(str(rapport * 255) + " ; ")
+                vrobot.write(str(vmax/r) + ",")
+                vrobot.write(str(rapport * vmax/r) + ";")
+                sleep(0.01)
             else:
-                vgauche.write(str(255) + " ; ")
-                vdroit.write(str(rapport * 255) + " ; ")
-        vgauche.write("\n")
-        vdroit.write("\n")
+                vrobot.write(str(rapport * vmax/r) + ",")
+                vrobot.write(str(vmax/r) + ";")
+                sleep(0.01)
 
         start = time()
 
         while time() - start < tau_plat:
             if dx_veh < 0:  # On tourne a gauche
-                vdroit.write(str(255 - 127 * (time() - start) * amax/vmax) + " ; ")
-                vgauche.write(str(255*rapport - rapport * 127 * (time() - start) * amax/vmax) + " ; ")
+                vrobot.write(str(vmax/r - (time() - start) * amax/r) + ",")
+                vrobot.write(str(vmax*rapport/r - rapport * (time() - start) * amax/r) + ";")
+                sleep(0.01)
             else:
-                vgauche.write(str(255 - 127 * (time() - start) * amax/vmax) + " ; ")
-                vdroit.write(str(255 * rapport - rapport * 127 * (time() - start) * amax/vmax) + " ; ")
-        vgauche.write("127")
-        vdroit.write("127")
+                vrobot.write(str(vmax/r * rapport - rapport * (time() - start) * amax/r) + ",")
+                vrobot.write(str(vmax/r - (time() - start) * amax/r) + ";")
+                sleep(0.01)
+        vrobot.write("0,")
+        vrobot.write("0")
 
         return 0
 
@@ -86,34 +117,33 @@ def arc(x,y,theta,xc,yc):
     else:
 
         start = time()
-
         while time() - start < tau:
-            if dx_veh > 0: #On tourne a gauche
-                vdroit.write(str(127 + 127*(time()-start)*amax)+ " ; ")
-                vgauche.write(str(127 + rapport*127*(time()-start)*amax)+" ; ")
+            if dx_veh < 0: #On tourne a gauche
+                
+                vrobot.write(str((time()-start)*amax/r)+ ",")
+                vrobot.write(str(rapport*(time()-start)*amax/r)+";")
+                sleep(0.01)
             else:
-                vgauche.write(str(127 + 127 * (time() - start) * amax) + " ; ")
-                vdroit.write(str(127 + rapport * 127 * (time() - start) * amax) + " ; ")
-        vgauche.write("\n")
-        vdroit.write("\n")
+                
+                vrobot.write(str(rapport * (time() - start) * amax/r) + ",")
+                vrobot.write(str((time() - start) * amax/r) + ";")
+                sleep(0.01)
 
         start = time()
 
         while time() - start < tau:
-            if dx_veh > 0:  # On tourne a gauche
-                vdroit.write(str(127 + 127*amax*tau/vmax - 127 * (time() - start) * amax) + " ; ")
-                vgauche.write(str(127 + rapport*(127*amax*tau/vmax - 127 * (time() - start) * amax)) + " ; ")
+            if dx_veh < 0:  # On tourne a gauche
+                vrobot.write(str(amax*tau/r - (time() - start) * amax/r) + ",")
+                vrobot.write(str(rapport*(amax*tau/r - (time() - start) * amax/r)) + ";")
+                sleep(0.01)
             else:
-                vgauche.write(str(127 + 127 * amax * tau / vmax - 127 * (time() - start) * amax) + " ; ")
-                vdroit.write(str(127 + rapport * (127 * amax * tau / vmax - 127 * (time() - start) * amax)) + " ; ")
-        vgauche.write("127")
-        vdroit.write("127")
+                vrobot.write(str(rapport * (amax * tau/r - (time() - start) * amax/r)) + ",")
+                vrobot.write(str(amax * tau/r - (time() - start) * amax/r) + ";")
+                sleep(0.01)
+        vrobot.write("0,")
+        vrobot.write("0")
 
         return 0
 
 
-arc(0,0,1.57,1.5,1.5)
-
-
-
-
+arc(0,0,-pi,2,2)
