576a82a2d896e6751871eb6453e86bd4c0ed900d
PAUVRE-Charlie <63854843+PAUVRE-Charlie@users.noreply.github.com>
2020-05-14 18:05:27 +0200

    Update ligne_droite.py

--- Trajectoire/ligne_droite.py
+++ Trajectoire/ligne_droite.py
@@ -2,7 +2,7 @@ from time import *
 from math import *
 
 ### Code de la route ###
-vmax = 1.0 #en m/s (en vrai je crois que c'est 0.42, a verifier)
+vmax = 1 #en m/s (en vrai je crois que c'est 0.42, a verifier)
 amax = 0.5 #en m/s^2 (la ca correspond a la valeur de r et epsmax
 
 ### Contraintes techniques ###
@@ -11,6 +11,7 @@ epsmax = 12.5 #en rad.s-2
 r = 0.04      #4cm
 L = 0.3       #30cm
 
+
 ### Variables utiles au prgm ###
 plat = 2
 start = 0
@@ -18,10 +19,11 @@ start = 0
 ### Valeurs caracteristiques
 tau = vmax/amax
 
+
 def ligne_droite (distance):
 
     global t_plat
-    fichier = open("vrobot", "w")
+    fichier = open("vrobot_ligne", "w")
 
     if distance > vmax*vmax/amax:
         plat = 1
@@ -33,36 +35,43 @@ def ligne_droite (distance):
 
     if plat == 1:
         while time()-start < tau:
-            fichier.write(str(127 + 128*(time()-start) * amax/vmax) + ";")
+            fichier.write(str((time()-start) * amax/r) + ",")
+            fichier.write(str((time()-start) * amax/r) + ";")
             sleep(0.01)
-        fichier.write("\n")
+        #fichier.write("\n")
         start = time()
         while time()-start < t_plat:
-            fichier.write(str(255) + ";")
+            fichier.write(str(vmax/r) + ",")
+            fichier.write(str(vmax/r) + ";")
             sleep(0.01)
-        fichier.write("\n")
+        #fichier.write("\n")
         start = time()
         while vmax > (time()-start)*amax:
-            fichier.write(str(127 + 128*(vmax-(time()-start)*amax)/vmax) + ";")
+            fichier.write(str((vmax-(time()-start)*amax)/r) + ",")
+            fichier.write(str((vmax-(time()-start)*amax)/r) + ";")
             sleep(0.01)
-        fichier.write(str(127))
+        fichier.write(str(0) + ",")
+        fichier.write(str(0))
         return 0
 
     elif plat == 0:
         while time()-start < sqrt(distance/amax):
-            fichier.write(str(127 + 128*(time() - start) * amax/vmax) + ";")
-            fichier.write(" ; ")
+            fichier.write(str((time() - start) * amax/r) + ",")
+            fichier.write(str((time() - start) * amax/r) + ";")
+            #fichier.write(" ; ")
             sleep(0.01)
-        fichier.write("\n")
+        #fichier.write("\n")
         start = time()
         while sqrt(distance*amax) > (time()-start)*amax:
-            fichier.write(str(127 + 128/vmax*(sqrt(amax*distance)-(time()-start)*amax)) + ";")
-            fichier.write(" ; ")
+            fichier.write(str((sqrt(amax*distance)-(time()-start)*amax)/r) + ",")
+            fichier.write(str((sqrt(amax*distance)-(time()-start)*amax)/r) + ";")
+            #fichier.write(" ; ")
             sleep(0.01)
-        fichier.write(str(127))
+        fichier.write(str(0) + ",")
+        fichier.write(str(0))
         return 0
 
     else:
         return 1
 
-ligne_droite(1)
+ligne_droite(3)
